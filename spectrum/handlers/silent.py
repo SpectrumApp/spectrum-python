ec452e77fcd9d188c57f3189943a4ceceea069f0
Flavio Curella <flavio.curella@gmail.com>
2015-11-05 15:47:04 -0600

    better silencing

--- spectrum/handlers/silent.py
+++ spectrum/handlers/silent.py
@@ -3,19 +3,16 @@ from concurrent.futures import ThreadPoolExecutor
 
 
 class DisableLogger():
-    def __init__(self, level):
-        self.level = level
-
     def __enter__(self):
-        logging.disable(self.level)
+        logging.shutdown()
 
     def __exit__(self, *args, **kwargs):
-        logging.disable(logging.NOTSET)
+        pass
 
 
 class SilentExecutor(ThreadPoolExecutor):
     def submit(self, func, *args, **kwargs):
         def wrap(*args_, **kwargs_):
-            with DisableLogger(logging.INFO):
+            with DisableLogger():
                 func(*args_, **kwargs_)
         return super(SilentExecutor, self).submit(wrap, *args, **kwargs)
