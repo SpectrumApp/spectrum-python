9b45744526934c19989e4fda30fbd3360c97cae7
Flavio Curella <flavio.curella@gmail.com>
2015-12-03 10:04:31 -0600

    add `fire_hose` configuration method

--- spectrum/django/spectrum.py
+++ spectrum/django/spectrum.py
@@ -149,3 +149,61 @@ FIRE_HOSE_UDP = {
         },
     },
 }
+
+
+def fire_hose(base_config=None, log_db=True, levels=None, handler_kwargs=None):
+    """
+
+    A convenience method to get and modify predefined logging configurations.
+
+    Arguments
+    ~~~~~~~~~
+
+    * ``base_config``: Defaults to `FIRE_HOSE`, which uses the REST HTTP stream on ``http://127.0.0.1:9000/``
+    * ``log_db``: shortcut for toggling the level of ``django.db.backends`` logging. Defaults to ``True``
+    * ``levels``: if provided, a 2-tuples iterable of logger names and their level.
+    * ``handler_kwargs``: if provided, kwargs to pass to the handles. Use this to override default settings such as ip / port Spectrum is running on.
+
+    Examples
+    ~~~~~~~~
+
+    ::
+
+        from spectrum.django import fire_hose, FIRE_HOSE_UDP
+
+
+        LOGGING = fire_hose()
+
+        LOGGING = fire_hose(log_db=False)
+
+        LOGGING = fire_hose(levels=(
+            ('my.overly.verbose.module', 'WARNING'),
+            ('some.other.module', 'CRITICAL'),
+        )
+
+        LOGGING = fire_hose(FIRE_HOSE_UDP, handler_kwargs={'url': '127.0.0.1:12345'})
+
+    """
+
+    if base_config is None:
+        base_config = FIRE_HOSE
+
+    if levels is None:
+        levels = tuple()
+
+    if handler_kwargs is None:
+        handler_kwargs = {}
+
+    if log_db is False:
+        base_config['loggers']['django.db.backends']['level'] = 'WARNING'
+
+    for silenced, level in levels:
+        if silenced not in base_config['loggers']:
+            base_config['loggers'][silenced] = {}
+
+        base_config['loggers'][silenced]['level'] = level
+
+    for handler, handler_config in base_config['handlers'].items():
+        handler_config.update(handler_kwargs)
+
+    return base_config
